7d2ebdab4fbd7b0a03e07c80f2b9f324bf42742b
MUHAMMADSHOAIBB <99481729+MUHAMMADSHOAIBB@users.noreply.github.com>
2022-04-01 10:45:55 +0500

    Add files via upload
    Task 5.1
    Ask user to enter his email that should have .com in the end
    Task 5.2
    Hello! ( user name ) .. should be titled I hope you are fine ,
    let me know how can i help you ! ( start asking ) if user say thanks , no thanks or i dont need any
    help
    then print ,
     Okay! Good to see you , stay connecteed
    but if user say yes, yes i need your help, then print
     We will resolve your problem
    Task 5.3
    ask user to enter his URL that should starts with http://www.( user url ) and then convert it into user
    url.com
    Task 5.4
    ask user to set your passwrd that should contains number , alphabets

---
+++ Task 5.py
@@ -0,0 +1,24 @@
+print('=================task 5.1==============')
+a1 = input('please enter your mail')
+print(a1.endswith('.com'))
+print('=================task5.2=================')
+print('Hello!')
+b1= input('enter title name')
+print(b1.title())
+print('i hope you are fine,let me know how can i help you')
+x = 'thanks'
+x1 = 'no thanks'
+x3 = 'i dont need any help'
+usser= x==x1  or x==x3
+print(usser,'ok good to see uh')
+
+
+print('===================Task5.3===============')
+c1=input('enter your URL')
+print(c1.startswith('https://www.'))
+print(c1.replace('https://www.','url.com'))
+print('========================TAsk5.4====================')
+user= input('please sir enter your password')
+user1= int(input('please sir enter your password'))
+print(user.isalpha())
+print(user.isnumeric)
